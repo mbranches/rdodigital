fd0fb42d516889d3b652324b26c86306d1978bdc
mbranches <marcusvbranches@gmail.com>
2025-11-30 13:39:36 -0300

    feat: adiciona validação obrigatória nos campos de configuração de relatórios de obra

--- src/main/java/com/branches/obra/dto/request/UpdateConfiguracoesDeRelatoriosDeObraRequest.java
+++ src/main/java/com/branches/obra/dto/request/UpdateConfiguracoesDeRelatoriosDeObraRequest.java
@@ -1,16 +1,29 @@
 package com.branches.obra.dto.request;
 
 import com.branches.configuradores.domain.enums.RecorrenciaRelatorio;
+import jakarta.validation.constraints.NotNull;
 
-public record UpdateConfiguracoesDeRelatoriosDeObraRequest(RecorrenciaRelatorio recorrenciaRelatorio,
-                                                           Boolean showCondicaoClimatica,
-                                                           Boolean showMaoDeObra,
-                                                           Boolean showEquipamentos,
-                                                           Boolean showAtividades,
-                                                           Boolean showOcorrencias,
-                                                           Boolean showComentarios,
-                                                           Boolean showMateriais,
-                                                           Boolean showHorarioDeTrabalho,
-                                                           Boolean showFotos,
-                                                           Boolean showVideos) {
+public record UpdateConfiguracoesDeRelatoriosDeObraRequest(
+        @NotNull(message = "O campo 'recorrenciaRelatorio' é obrigatório")
+        RecorrenciaRelatorio recorrenciaRelatorio,
+        @NotNull(message = "O campo 'showCondicaoClimatica' é obrigatório")
+        Boolean showCondicaoClimatica,
+        @NotNull(message = "O campo 'showMaoDeObra' é obrigatório")
+        Boolean showMaoDeObra,
+        @NotNull(message = "O campo 'showEquipamentos' é obrigatório")
+        Boolean showEquipamentos,
+        @NotNull(message = "O campo 'showAtividades' é obrigatório")
+        Boolean showAtividades,
+        @NotNull(message = "O campo 'showOcorrencias' é obrigatório")
+        Boolean showOcorrencias,
+        @NotNull(message = "O campo 'showComentarios' é obrigatório")
+        Boolean showComentarios,
+        @NotNull(message = "O campo 'showMateriais' é obrigatório")
+        Boolean showMateriais,
+        @NotNull(message = "O campo 'showHorarioDeTrabalho' é obrigatório")
+        Boolean showHorarioDeTrabalho,
+        @NotNull(message = "O campo 'showFotos' é obrigatório")
+        Boolean showFotos,
+        @NotNull(message = "O campo 'showVideos' é obrigatório")
+        Boolean showVideos) {
 }

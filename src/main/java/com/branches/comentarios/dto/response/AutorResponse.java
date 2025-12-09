62bf64ada0cfb444c4409ed6639e3cdafa3d7137
mbranches <marcusvbranches@gmail.com>
2025-12-09 17:55:23 -0300

    feat: adiciona AutorResponse e atualiza ComentarioDeRelatorioResponse com autor e data de criação

---
+++ src/main/java/com/branches/comentarios/dto/response/AutorResponse.java
@@ -0,0 +1,17 @@
+package com.branches.comentarios.dto.response;
+
+import com.branches.user.domain.UserEntity;
+
+public record AutorResponse(
+        Long id,
+        String nome,
+        String email
+) {
+    public static AutorResponse from(UserEntity autorEntity) {
+        return new AutorResponse(
+                autorEntity.getId(),
+                autorEntity.getNome(),
+                autorEntity.getEmail()
+        );
+    }
+}
--- src/main/java/com/branches/comentarios/dto/response/ComentarioDeRelatorioResponse.java
+++ src/main/java/com/branches/comentarios/dto/response/ComentarioDeRelatorioResponse.java
@@ -3,11 +3,14 @@ package com.branches.comentarios.dto.response;
 import com.branches.comentarios.model.ComentarioDeRelatorioEntity;
 import com.branches.relatorio.dto.response.CampoPersonalizadoResponse;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public record ComentarioDeRelatorioResponse(
         Long id,
         String descricao,
+        AutorResponse autor,
+        LocalDateTime dataCriacao,
         List<CampoPersonalizadoResponse> camposPersonalizados
 ) {
     public static ComentarioDeRelatorioResponse from(ComentarioDeRelatorioEntity comentarioDeRelatorioEntity) {
@@ -19,6 +22,8 @@ public record ComentarioDeRelatorioResponse(
         return new ComentarioDeRelatorioResponse(
                 comentarioDeRelatorioEntity.getId(),
                 comentarioDeRelatorioEntity.getDescricao(),
+                AutorResponse.from(comentarioDeRelatorioEntity.getAutor()),
+                comentarioDeRelatorioEntity.getDataCriacao(),
                 camposPersonalizadosResponse
         );
     }

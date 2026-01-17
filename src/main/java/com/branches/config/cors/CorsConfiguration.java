22245fb54e23e35444df439477b416324663dd7f
mbranches <marcusvbranches@gmail.com>
2026-01-16 23:21:24 -0300

    fat: atualiza cors

--- src/main/java/com/branches/config/cors/CorsConfiguration.java
+++ src/main/java/com/branches/config/cors/CorsConfiguration.java
@@ -10,7 +10,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://meudiariodeobras.vercel.app/", "http://meudiariodeobras.vercel.app/")
+                .allowedOrigins("http://localhost:3000", "https://rdodigital.com.br")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT", "PATCH")
                 .exposedHeaders("Content-Disposition");
     }

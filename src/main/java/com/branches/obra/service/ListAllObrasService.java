7eec1e899eb71eda6b10c4766792d15b2552e7d8
mbranches <marcusvbranches@gmail.com>
2026-01-18 11:51:16 -0300

    refactor: atualiza ci e prazo decorrido

--- src/main/java/com/branches/obra/service/ListAllObrasService.java
+++ src/main/java/com/branches/obra/service/ListAllObrasService.java
@@ -3,6 +3,7 @@ package com.branches.obra.service;
 import com.branches.obra.dto.response.ObraByListAllResponse;
 import com.branches.obra.repository.ObraRepository;
 import com.branches.obra.repository.projections.ObraProjection;
+import com.branches.shared.calculators.CalculatePrazoDecorrido;
 import com.branches.tenant.service.GetTenantIdByIdExternoService;
 import com.branches.usertenant.domain.UserTenantEntity;
 import com.branches.usertenant.domain.enums.PerfilUserTenant;
@@ -11,9 +12,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 import static com.branches.usertenant.domain.enums.PerfilUserTenant.ADMINISTRADOR;
@@ -24,6 +23,7 @@ public class ListAllObrasService {
     private final ObraRepository obraRepository;
     private final GetTenantIdByIdExternoService getTenantIdByIdExternoService;
     private final GetCurrentUserTenantService getCurrentUserTenantService;
+    private final CalculatePrazoDecorrido calculatePrazoDecorrido;
 
     public List<ObraByListAllResponse> execute(String tenantExternalId, List<UserTenantEntity> userTenants) {
         Long tenantId = getTenantIdByIdExternoService.execute(tenantExternalId);
@@ -41,14 +41,8 @@ public class ListAllObrasService {
                     LocalDate startDate = o.getDataInicio();
                     LocalDate endDate = o.getDataPrevistaFim();
 
-                    long prazoTotal = Math.max(ChronoUnit.DAYS.between(startDate, endDate), 1);
-
                     LocalDate dataFimToReferencia = o.getDataFimReal() != null ? o.getDataFimReal() : LocalDate.now();
-                    long prazoDecorrido = ChronoUnit.DAYS.between(startDate, dataFimToReferencia);
-                    BigDecimal prazoPercentualDecorrido = prazoTotal - prazoDecorrido <= 0 ? BigDecimal.valueOf(100)
-                            : BigDecimal.valueOf(prazoDecorrido)
-                            .multiply(BigDecimal.valueOf(100))
-                            .divide(BigDecimal.valueOf(prazoTotal), 2, RoundingMode.HALF_UP);
+                    BigDecimal prazoPercentualDecorrido = calculatePrazoDecorrido.execute(startDate, endDate, dataFimToReferencia);
 
                     return ObraByListAllResponse.from(o, prazoPercentualDecorrido);
 
